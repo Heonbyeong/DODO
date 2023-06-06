18edb1d37d91332e85804ab959d9dabf682fe8e6
Heon <abh0920one@gmail.com>
2023-06-07 01:01:13 +0900

    #1 폰트 추가

--- app/src/main/java/com/example/dodo/MainActivity.kt
+++ app/src/main/java/com/example/dodo/MainActivity.kt
@@ -9,8 +9,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.dodo.ui.theme.DODOTheme
+import com.example.dodo.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,7 +35,8 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        style = Typography.bodyLarge
     )
 }
 
--- app/src/main/java/com/example/dodo/ui/theme/Type.kt
+++ app/src/main/java/com/example/dodo/ui/theme/Type.kt
@@ -2,33 +2,30 @@ package com.example.dodo.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.dodo.R
 
-// Set of Material typography styles to start with
+val notosanskr = FontFamily(
+    Font(R.font.notosanskr_black, FontWeight.Black, FontStyle.Normal),
+    Font(R.font.notosanskr_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.notosanskr_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.notosanskr_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.notosanskr_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.notosanskr_thin, FontWeight.Thin, FontStyle.Normal)
+)
+
+val cherrybomb = FontFamily(
+    Font(R.font.cherrybombone, FontWeight.Normal, FontStyle.Normal)
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = cherrybomb,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
