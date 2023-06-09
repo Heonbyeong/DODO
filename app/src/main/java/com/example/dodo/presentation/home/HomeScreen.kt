85a5c1fe941112a5c83871a907a11afb39ee4c9f
Heon <abh0920one@gmail.com>
2023-06-09 18:18:03 +0900

    #8 Todo List Add View 개발

--- app/src/main/java/com/example/dodo/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/dodo/presentation/home/HomeScreen.kt
@@ -1,8 +1,10 @@
 package com.example.dodo.presentation.home
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -12,9 +14,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.dodo.presentation.home.components.calendar.HorizontalCalendar
+import com.example.dodo.presentation.home.components.todolist.HomeTodoListAddView
 import com.example.dodo.presentation.home.components.todolist.HomeTodoListEmptyView
 import com.example.dodo.presentation.home.components.todolist.HomeTodoListItem
 import com.example.dodo.ui.theme.RegularC32
@@ -32,7 +36,8 @@ fun HomeScreen() {
             style = RegularC32
         )
         LazyColumn(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             item {
                 var selectDate by remember { mutableStateOf("") } // TODO 테스트 용
@@ -46,6 +51,10 @@ fun HomeScreen() {
             if (todoList.isNotEmpty()) {
                 itemsIndexed(items = todoList) { index, data ->
                     HomeTodoListItem(modifier = Modifier.fillMaxWidth())
+                }
+                item {
+                    HomeTodoListAddView(modifier = Modifier.padding(top = 20.dp))
+                    Spacer(modifier = Modifier.height(75.dp))
                 }
             } else {
                 item {
---
+++ app/src/main/java/com/example/dodo/presentation/home/components/todolist/HomeTodoListAddView.kt
@@ -0,0 +1,48 @@
+package com.example.dodo.presentation.home.components.todolist
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.dodo.R
+import com.example.dodo.ui.theme.MediumN12
+import com.example.dodo.ui.theme.gray0
+import com.example.dodo.ui.theme.gray09
+
+@Composable
+fun HomeTodoListAddView(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .clip(shape = RoundedCornerShape(30.dp))
+            .background(gray0),
+        contentAlignment = Alignment.Center
+    ) {
+        Row(
+            modifier = Modifier.padding(horizontal = 15.dp, vertical = 7.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "추가",
+                style = MediumN12,
+                color = gray09
+            )
+            Icon(
+                modifier = Modifier.padding(start = 5.dp),
+                painter = painterResource(id = R.drawable.ic_todo_add),
+                contentDescription = null,
+                tint = gray09
+            )
+        }
+    }
+}
