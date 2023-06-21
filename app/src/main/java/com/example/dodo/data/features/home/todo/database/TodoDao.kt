18e697c928c9122ab43eabbb62e6884406ec3909
Heon <abh0920one@gmail.com>
2023-06-21 18:44:45 +0900

    #18 Todo Dao add

---
+++ app/src/main/java/com/example/dodo/data/features/home/todo/database/TodoDao.kt
@@ -0,0 +1,36 @@
+package com.example.dodo.data.features.home.todo.database
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TodoDao {
+
+    @Query("SELECT * FROM todoList")
+    suspend fun fetchTodoList(): List<Todo>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addTodo(todo: Todo)
+
+    @Update
+    suspend fun editTodo(todo: Todo)
+
+    @Delete
+    suspend fun deleteTodo(todo: Todo)
+
+    @Query("DELETE FROM todoList WHERE id = :id")
+    suspend fun deleteTodo(id: Long)
+
+    @Query("UPDATE todoList SET isDone = :isDone WHERE id = :id")
+    suspend fun doneTodo(id: Long, isDone: Boolean = true)
+
+    @Query("UPDATE todoList SET isNotify = :isNotify WHERE id = :id")
+    suspend fun notifyChange(id: Long, isNotify: Boolean = false)
+
+    @Query("SELECT * FROM todoList WHERE id = :id")
+    suspend fun fetchTodoDetail(id: Long)
+}
