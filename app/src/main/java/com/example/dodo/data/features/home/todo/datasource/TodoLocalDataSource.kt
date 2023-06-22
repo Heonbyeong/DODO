cd7bc93d07fcb46271a8b5d7e7c7c0b70e924985
Heon <abh0920one@gmail.com>
2023-06-23 00:37:54 +0900

    #20 Todo datasource 개발

---
+++ app/src/main/java/com/example/dodo/data/features/home/todo/datasource/TodoLocalDataSource.kt
@@ -0,0 +1,20 @@
+package com.example.dodo.data.features.home.todo.datasource
+
+import com.example.dodo.data.features.home.todo.database.Todo
+
+interface TodoLocalDataSource {
+
+    suspend fun fetchTodoList(): List<Todo>
+
+    suspend fun addTodo(todo: Todo)
+
+    suspend fun editTodo(todo: Todo)
+
+    suspend fun deleteTodo(todo: Todo)
+
+    suspend fun doneTodo(id: Int, isDone: Boolean)
+
+    suspend fun notifyChange(id: Int, isNotify: Boolean)
+
+    suspend fun fetchTodoDetail(id: Int): Todo
+}
