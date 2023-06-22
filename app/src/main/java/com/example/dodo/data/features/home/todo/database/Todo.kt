474f1be79709905e719185e2963c22ab1e66ad41
Heon <abh0920one@gmail.com>
2023-06-23 00:41:54 +0900

    #20 Todo Entity import delete

--- app/src/main/java/com/example/dodo/data/features/home/todo/database/Todo.kt
+++ app/src/main/java/com/example/dodo/data/features/home/todo/database/Todo.kt
@@ -1,6 +1,5 @@
 package com.example.dodo.data.features.home.todo.database
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.time.LocalDate
