7811d49c0a492c0a69df8938e00e1974c4f87404
Heon <abh0920one@gmail.com>
2023-06-28 21:14:30 +0900

    #20 BaseViewModel 개발

--- app/src/main/java/com/example/dodo/presentation/base/BaseViewModel.kt
+++ app/src/main/java/com/example/dodo/presentation/base/BaseViewModel.kt
@@ -4,6 +4,4 @@ import androidx.lifecycle.ViewModel
 import org.orbitmvi.orbit.ContainerHost
 
 abstract class BaseViewModel<S: State, E: SideEffect> : ContainerHost<S, E>, ViewModel() {
-
-    abstract fun sendEvent(event: E)
 }
