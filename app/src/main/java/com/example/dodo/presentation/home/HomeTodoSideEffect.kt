9d1dfabd70f05dbf440475c176657b0cee083514
Heon <abh0920one@gmail.com>
2023-06-28 21:14:49 +0900

    #20 Home state, sideeffect 개발

--- app/src/main/java/com/example/dodo/presentation/home/HomeTodoSideEffect.kt
+++ app/src/main/java/com/example/dodo/presentation/home/HomeTodoSideEffect.kt
@@ -1,7 +1,8 @@
 package com.example.dodo.presentation.home
 
 import com.example.dodo.presentation.base.SideEffect
+import java.time.LocalDate
 
 sealed class HomeTodoSideEffect : SideEffect {
-
+    data class SelectedDate(val date: LocalDate) : HomeTodoSideEffect()
 }
--- app/src/main/java/com/example/dodo/presentation/home/HomeTodoState.kt
+++ app/src/main/java/com/example/dodo/presentation/home/HomeTodoState.kt
@@ -1,8 +1,11 @@
 package com.example.dodo.presentation.home
 
+import com.example.dodo.data.features.home.todo.database.Todo
 import com.example.dodo.presentation.base.State
 import java.time.LocalDate
 
 data class HomeTodoState(
-    val selectedDate: LocalDate = LocalDate.now()
-): State
+    val selectedDate: LocalDate = LocalDate.now(),
+    val todo: Todo? = null,
+) : State {
+}
