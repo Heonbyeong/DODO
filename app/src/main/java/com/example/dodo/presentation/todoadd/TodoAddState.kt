33857177e55e75bb2f78c941aa6825e519b36273
Heon <abh0920one@gmail.com>
2023-07-03 21:23:44 +0900

    #26 Reverse geocoding 비즈니스 로직 개발

--- app/src/main/java/com/example/dodo/presentation/todoadd/TodoAddState.kt
+++ app/src/main/java/com/example/dodo/presentation/todoadd/TodoAddState.kt
@@ -4,7 +4,11 @@ import com.example.dodo.presentation.common.BottomSheetScreen
 import com.example.dodo.presentation.base.State
 
 data class TodoAddState(
-    val currentSheet: BottomSheetScreen = BottomSheetScreen.TodoAddMapBottomSheet
+    val currentSheet: BottomSheetScreen = BottomSheetScreen.TodoAddMapBottomSheet,
+    val longitude: Double = 0.0,
+    val latitude: Double = 0.0,
+    val newAddress: String = "",
+    val oldAddress: String = ""
 ) : State {
 
 }
