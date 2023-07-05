9b2a9ab3135d96dc4280de009cb07c85c5029af0
Heon <abh0920one@gmail.com>
2023-07-05 15:13:39 +0900

    #26 permissions check and request

--- app/src/main/java/com/example/dodo/presentation/main/MainScreen.kt
+++ app/src/main/java/com/example/dodo/presentation/main/MainScreen.kt
@@ -1,5 +1,11 @@
 package com.example.dodo.presentation.main
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,8 +16,11 @@ import androidx.compose.material.ripple.RippleAlpha
 import androidx.compose.material.ripple.RippleTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.dodo.presentation.Navigation
@@ -19,6 +28,31 @@ import com.example.dodo.presentation.Navigation
 @Composable
 fun MainScreen(navController: NavController) {
     val navHostController = rememberNavController()
+    val context = LocalContext.current
+    val permissions = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_BACKGROUND_LOCATION
+    )
+    val launcherMultiplatform = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions()
+    ) {
+        val isGranted = it.values.reduce { acc, next -> acc && next }
+        if (isGranted) {
+            // TODO
+        } else {
+            // TODO
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        checkAndRequestPermissions(
+            context = context,
+            permissions = permissions,
+            launcher = launcherMultiplatform
+        )
+    }
+
     Scaffold(
         bottomBar = {
             CompositionLocalProvider(LocalRippleTheme provides NoRippleTheme) {
@@ -40,6 +74,23 @@ fun MainScreen(navController: NavController) {
             }
         }
     )
+}
+
+fun checkAndRequestPermissions(
+    context: Context,
+    permissions: Array<String>,
+    launcher: ManagedActivityResultLauncher<Array<String>, Map<String, Boolean>>
+) {
+    val hasPermissions = permissions.all {
+        ContextCompat.checkSelfPermission(context, it) ==
+                PackageManager.PERMISSION_GRANTED
+    }
+    // Already granted permissions
+    if (hasPermissions) {
+        // TODO
+    } else {
+        launcher.launch(permissions)
+    }
 }
 
 private object NoRippleTheme : RippleTheme {
